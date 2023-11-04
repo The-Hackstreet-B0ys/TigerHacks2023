1a5b6740c7ad1a8e68cabb805395b02ab1f6947f
nnnolan <pestanonolan@gmail.com>
2023-11-04 18:34:04 -0500

    working home page

--- insightsai/src/script.js
+++ insightsai/src/script.js
@@ -1,17 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   const paragraphs = document.querySelectorAll('.paragraph');
 
-  // Function to change the color to red when clicked
-  function changeColorToRed(event) {
-    const paragraph = event.currentTarget;
-    paragraph.style.color = 'red';
-  }
-
+  //fade out paragraphs to background
   function fadeToBackGround(event) {
     const paragraph = event.currentTarget;
     paragraph.fadeOut(2000, function() {
-      // Once the fade out is complete, you can perform additional actions here if needed
-    });
+      // Once the fade out is complete, you can perform additional actions here if needed 
+       });
   }
 
   // Add a click event listener to each paragraph and set pointer-events to "auto"
